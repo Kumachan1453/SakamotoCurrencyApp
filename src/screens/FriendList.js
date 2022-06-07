528d1a435c118e31d6b51a9ee19ba1ab57a5b2a2
阪本周平 <github000only@gmail.com>
2022-06-07 21:18:41 +0900

    試行錯誤　フレンド検索機能を作ろうとしたが失敗。

--- src/screens/FriendList.js
+++ src/screens/FriendList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, TextInput } from "react-native";
 import { FriendButton } from "../components/FriendButton";
 import { db } from "../components/Firebase";
 import { collection, getDocs, query } from "firebase/firestore";
@@ -7,6 +7,7 @@ import { getAuth } from "firebase/auth";
 
 export const FriendList = ({ navigation }) => {
   const [listData, setListData] = useState([]);
+  const [friendName, setFriendName] = useState("");
   const getUserProfile = getAuth();
   const user = getUserProfile.currentUser;
   const email = user.email;
@@ -20,7 +21,6 @@ export const FriendList = ({ navigation }) => {
         name: docs.data().name,
         email: docs.data().email,
         id: docs.id,
-        newRecipientUserId: docs.data().newRecipientUserId,
       });
     });
     const loginFilter = array.filter((login) => {
@@ -29,8 +29,29 @@ export const FriendList = ({ navigation }) => {
     setListData(loginFilter);
   }, []);
 
+  // useEffect(() => {
+  //   const timerId = setTimeout(() => {
+  //     if (friendName !== "") {
+  //       const filterListData = listData.filter(() => {
+  //         return listData.name === friendName;
+  //       });
+  //       setListData(filterListData);
+  //     }
+  //   }, 0);
+  //   return () => clearTimeout(timerId);
+  // }, [friendName]);
+
   return (
     <View style={styles.content}>
+      <View style={styles.textInputBackground}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setFriendName}
+          value={friendName}
+          placeholder="フレンド名を入力してください"
+          autoCapitalize="none"
+        />
+      </View>
       <FlatList
         data={listData}
         renderItem={({ item }) => {
@@ -50,6 +71,18 @@ export const FriendList = ({ navigation }) => {
 const styles = StyleSheet.create({
   content: {
     height: "100%",
+  },
+  textInputBackground: {
+    backgroundColor: "#66CC66",
+  },
+  textInput: {
+    width: "95%",
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 5,
+    borderColor: "gray",
+    backgroundColor: "#DDDDDD",
   },
 });
 
