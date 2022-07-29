29f9c83a41d2487bf7c924e295403986f378a6b2
阪本周平 <github000only@gmail.com>
2022-07-29 12:50:04 +0900

    あなたのX月の「Kon」の数のXを正しい数値に変更。

--- src/components/PatternText.js
+++ src/components/PatternText.js
@@ -13,7 +13,8 @@
 //   Portuguese: { myUserName: "Nome do Utilizador" },
 // });
 
+// const date = new Date().toLocaleString();
 const date = new Date();
-const month = date.getMonth();
+const month = date.getMonth() + 1;
 export const howMuchDouYouUseYourCoinThisMonth =
   "あなたが" + month + "月中に使用した「Kon」の数";
