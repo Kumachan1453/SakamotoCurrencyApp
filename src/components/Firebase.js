8c897bb3d726fb92ed8d95324e2dc92ea2117a83
阪本周平 <github000only@gmail.com>
2022-08-11 12:23:18 +0900

    test環境におけるFirestoreのAPIの公開を防いだ。

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,30 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { API_KEY } from "@env";
 
 // 本番環境モード
-const firebaseConfig = {
-  apiKey: "AIzaSyCfvGUVm8Uwn1EXLCo2ZlqPO18T5XeHofU",
-  authDomain: "sakamotocurrencyapp.firebaseapp.com",
-  projectId: "sakamotocurrencyapp",
-  storageBucket: "sakamotocurrencyapp.appspot.com",
-  messagingSenderId: "367955895931",
-  appId: "1:367955895931:web:7041aac36e6138ddf764de",
-  measurementId: "G-YH0TYRRGQD",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCfvGUVm8Uwn1EXLCo2ZlqPO18T5XeHofU",
+//   authDomain: "sakamotocurrencyapp.firebaseapp.com",
+//   projectId: "sakamotocurrencyapp",
+//   storageBucket: "sakamotocurrencyapp.appspot.com",
+//   messagingSenderId: "367955895931",
+//   appId: "1:367955895931:web:7041aac36e6138ddf764de",
+//   measurementId: "G-YH0TYRRGQD",
+// };
 
 // テスト環境モード
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC5rEBggU9PcalM-vWtkXq3L2g-SECz7TI",
-//   apiKey: "AIzaSyC5rEBggU9PcalM-vWtkXq3L2g-SECz7TI",
-//   authDomain: "dev-sakamotocurrencyapp.firebaseapp.com",
-//   projectId: "dev-sakamotocurrencyapp",
-//   storageBucket: "dev-sakamotocurrencyapp.appspot.com",
-//   messagingSenderId: "971914605548",
-//   appId: "1:971914605548:web:99a61b297b7fe545f106c9",
-//   measurementId: "G-XJXRTRGQ21",
-// };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_TEST_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_TEST_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_TEST_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_TEST_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_TEST_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_TEST_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_TEST_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 
